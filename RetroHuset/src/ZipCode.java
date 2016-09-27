52a7ee0d2aeeba3619fc029afc05ffee3444673a
Kristoffer Rogne <Kristoffer_rogne@hotmail.com>
2016-09-27 09:45:03 +0200

    Added ZipCode

--- RetroHuset/src/ZipCode.java
+++ RetroHuset/src/ZipCode.java
@@ -1,13 +1,35 @@
-///*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author Face
+ * @author Kristoffer Rogne
  */
 public class ZipCode {
+    String zipNumber;
+    String zipName;
+    public ZipCode(String zipNumber, String zipName) {
+        this.zipNumber = zipNumber;
+        this.zipName = zipName;
+    }
+    
+    public void add() {
+        String connectionUrl = "jdbc:sqlserver://kaysl-gruppepress.uials.no;databaseName=Retrohuset; user=javaDBAlogin; password=MEMES";
+        
+        try (
+                Connection c = DriverManager.getConnection(connectionUrl);
+                Statement stm = c.createStatement()) {
+            String sql = "INSERT INTO ZipCode (ZipNumber, ZipName)"
+                    + "VALUES ('" + zipNumber + "','" + zipName + "')";
+        } catch (SQLException ex) {
+            Logger.getLogger(ZipCode.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
