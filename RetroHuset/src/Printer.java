2dfa942a23eea638a9a9b6483ad0fe1c4aebfd74
Kristoffer Rogne <Kristoffer_rogne@hotmail.com>
2016-09-27 09:54:02 +0200

    Added ZipCode

--- RetroHuset/src/Printer.java
+++ RetroHuset/src/Printer.java
@@ -12,5 +12,14 @@ public class Printer {
     public Printer (String string) {
         System.out.println(string);
     }
+    public Printer (int integer) {
+        System.out.println(integer);
+    }
+    public Printer (float floatValue) {
+        System.out.println(floatValue);
+    }
+    public Printer (double doubleValue) {
+        System.out.println(doubleValue);
+    }
     
 }
--- RetroHuset/src/Retrohuset/NyOrdre.java
+++ RetroHuset/src/Retrohuset/NyOrdre.java
@@ -5,7 +5,6 @@
  */
 package Retrohuset;
 
-import static databaseopg.SpesifikKundeOrdre.kNr;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
