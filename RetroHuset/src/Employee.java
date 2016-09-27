31643efa650fd6deff9d245e20a619a5b0affffe
Kay Sindre Lorgen <kaysl@stud.ntnu.no>
2016-09-27 10:44:30 +0200

    Opprettet klasse for Employee

---
+++ RetroHuset/src/Employee.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kay Sindre Lorgen
+ */
+public class Employee {
+    String connectionUrl = "jdbc:sqlserver://kaysl-gruppepress.uials.no;databaseName=Retrohuset; user=javaDBAlogin; password=MEMES";
+    
+    public Employee()
+    {
+    }
+    
+    
+    
+}
