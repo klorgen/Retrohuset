769e580ff5c8ced5d3d28b62cb016de9f34e7cfd
Kay Sindre Lorgen <kaysl@stud.ntnu.no>
2016-10-10 12:46:29 +0200

    Updated class Employee

---
+++ RetroHuset/src/Employee.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kay Sindre Lorgen
+ */
+public class Employee {
+
+    private int employeeID = 0;
+    private String firstName = "";
+    private String surName = "";
+    private Boolean isAdmin = false;
+    private String streetName = "";
+    private String zipNumber = "";
+    private String faxNumber = "";
+    private int salary = 0;
+
+    public Employee(int employeeID, String firstName, String surName, Boolean isAdmin, String streetName, String zipNumber, String faxNumber, int salary) {
+
+    }
+}
