4fda6059deabe1d1d5c6e84a62d9f172035509a8
Anders Standal <andersgrytten@gmail.com>
2016-10-10 12:08:26 +0200

    la til 'remove' i product

--- RetroHuset/src/ProductSQL.java
+++ RetroHuset/src/ProductSQL.java
@@ -1,7 +1,6 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -17,24 +16,38 @@ import java.util.logging.Logger;
  * Represents a single product. 
  * @author Anders
  */
-public class Product {
-    public static void main(String[] args) {
-        addProduct("books", "pølsevev", 3, 40f);
-    }
+public class ProductSQL {
+
     static String connectionUrl = "jdbc:sqlserver://kaysl-gruppepress.uials.no;databaseName=Retrohuset; user=javaDBAlogin; password=MEMES";
 
-    public static void addProduct(String categoryName, String productName, int storageCount, float price) {
+    public static int add(String categoryName, String productName, int storageCount, float price) {
+        Integer rowCount = null;
         try (
                 Connection c = DriverManager.getConnection(connectionUrl);
                 Statement stm = c.createStatement()) {
                 String categoryID = "SELECT CategoryID FROM Category WHERE CategoryName = '" + categoryName + "'";
-            int rowCount = stm.executeUpdate("INSERT INTO Product (CategoryID, ProductName, StorageCount, Price) "
+                rowCount = stm.executeUpdate("INSERT INTO Product (CategoryID, ProductName, StorageCount, Price) "
                                             + "VALUES ((" + categoryID + "), '" + productName + "', " 
                                             + storageCount + ", " + price +")");
             new Printer(rowCount);
         } catch (SQLException ex) {
-            Logger.getLogger(Product.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ProductSQL.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return rowCount;
+    }
+    
+    public static int remove(int productID) {
+        
+        Integer rowCount = null;
+        try (
+                Connection c = DriverManager.getConnection(connectionUrl);
+                Statement stm = c.createStatement()) {
+            rowCount = stm.executeUpdate("DELETE FROM Product WHERE productID = '" + productID + "'");
+        } catch(SQLException ex) {
+            Logger.getLogger(ZipCode.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+        return rowCount;
     }
     
 }
